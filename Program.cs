548658207c4086b53c1c9ccc661f42bfdb2d7fa6
Evgeniy Neronov <e.neronov@gmail.com>
2023-05-18 01:42:45 +0300

    example with docker-compose

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using NLog;
+
+class Program
+{
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    static void Main()
+    {
+        var config = new NLog.Config.XmlLoggingConfiguration("NLog.config");
+        NLog.LogManager.Configuration = config;
+        
+        Console.WriteLine(string.Join(", ", config.AllTargets));
+
+        Logger.Trace("Это проверочный лог");
+        LogManager.Flush();
+        LogManager.Shutdown();
+    }
+}
